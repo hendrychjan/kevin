451e0cf375ec60bacf574e68733c0556446a9e9d
hendrychjan <jan.honza.hendrych@gmail.com>
2022-12-16 14:17:00 +0100

    Remove -negate flags in image conversion

--- components/img-convert/convert.sh
+++ components/img-convert/convert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir out 2>/dev/null
 magick maze.png -sample 100x100 out/maze-resized.png
-magick out/maze-resized.png -color-threshold 'sRGB(0,0,0)-sRGB(160,160,160)' -negate out/map.png
-magick out/maze-resized.png -color-threshold 'sRGB(0,0,200)-sRGB(160,160,255)' -negate out/start.png
-magick out/maze-resized.png -color-threshold 'sRGB(200,0,0)-sRGB(255,160,160)' -negate out/end.png
+magick out/maze-resized.png -color-threshold 'sRGB(0,0,0)-sRGB(160,160,160)' out/map.png
+magick out/maze-resized.png -color-threshold 'sRGB(0,0,200)-sRGB(160,160,255)' out/start.png
+magick out/maze-resized.png -color-threshold 'sRGB(200,0,0)-sRGB(255,160,160)' out/end.png
 rm out/maze-resized.png
